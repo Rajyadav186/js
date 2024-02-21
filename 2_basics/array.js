9b3e2dad2d4cc7927c1d826ddc98461e6a92b42f
Raj Yadav <118369121+Rajyadav186@users.noreply.github.com>
2024-02-21 17:47:25 +0000

    saved

---
+++ 2_basics/array.js
@@ -0,0 +1,36 @@
+//Array
+
+const number=[0,1,2,3,4,5]
+const string=['a','b','c','d','e','f'];
+console.log(string[2])
+
+//Array Method
+
+// number.push(6)
+
+// console.log(number)
+// number.pop()
+// console.log(number)
+
+// number.unshift(-1)
+// console.log(number)
+// number.shift()
+// console.log(number)
+
+
+// console.log(number.indexOf(3))
+
+// const newNumber=number.join()
+// console.log( number)
+// console.log( newNumber)
+
+//   Slice,Splice
+
+const number1=number.slice(1,3)
+console.log("A=",number)
+console.log(number1);
+console.log('B=',number);
+
+const number2=number.splice(1,3)
+console.log(number2);
+console.log("C=",number);
