0e54b374c91cb58eef4105e56efe019f47d0452e
Raj Yadav <118369121+Rajyadav186@users.noreply.github.com>
2024-02-22 18:13:54 +0000

    saved

---
+++ 2_basics/2array.js
@@ -0,0 +1,16 @@
+const mcu=['Iron Man','Thor','Spider Man']
+const dc=['Super Man','Flash','Batman']
+
+
+//+++++Push ++++
+// mcu.push(dc)
+// console.log(mcu);
+// console.log(mcu[3][0]);
+
+
+//+++Concatinate+++++
+// const multiverse=mcu.concat(dc)
+// console.log(multiverse);
+
+
+//++++Spred oprater++++++
