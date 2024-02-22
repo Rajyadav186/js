1f3282cb0e6b6c6412bf0d41a4c2a2842c755fef
Raj Yadav <118369121+Rajyadav186@users.noreply.github.com>
2024-02-22 18:32:17 +0000

    saved

--- 2_basics/2array.js
+++ 2_basics/2array.js
@@ -13,4 +13,22 @@ const dc=['Super Man','Flash','Batman']
 // console.log(multiverse);
 
 
-//++++Spred oprater++++++
+// //++++Spred oprater++++++
+// const multiverse=[...mcu,...dc];
+// console.log(multiverse)
+
+
+const arr=[1,2,3,[4,5,6],7,[8,9,0],[34,64,[34,75,34],34]];
+const usable_another_arr=arr.flat(Infinity)
+console.log(usable_another_arr)
+
+console.log(Array.isArray('Raj'))
+console.log(Array.from('Raj'))
+console.log(Array.from({name:'raj'}))
+
+let score1=123
+let score2=175
+let score3=164
+let score4=186
+
+console.log(Array.of(score1,score2,score3,score4));
